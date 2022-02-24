405d975ece1e658ca7edf6670d4f9e653f11b81d
Nhoi <pepo01gl@gmail.com>
2022-02-24 12:06:24 +0700

    oklkkkk

--- src/components/QLHP/Page/CTHV/CTHVien.tsx
+++ src/components/QLHP/Page/CTHV/CTHVien.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "../CTHV/CTHVien.css";
 
 export default function CTHVien() {
   return (
@@ -7,7 +8,55 @@ export default function CTHVien() {
             <div className='table-heading'>
                 <div className='table-nav'>
                     <div className='table-select'>
+                        <div className='table-details-class'>
+                            <div className='box-details'>
+                                <div className='title-heading-cthv'>Thông tin học viên</div>
+                                <div className='table-select-cthv'>
+                                    <h3 className='details-hvt'>Họ và tên :</h3>
+                                    <p className='details-class-cthv font-size'>Bùi Thế Anh </p>
+                                    <h3 className='details-cmnd'>CMND:</h3>
+                                    <p className='details-class-cthv font-size'>123 1234 556</p>  
+                                    <h3 className='details-lhdt'>Loại hình đào tạo:</h3>
+                                    <p className='details-class-cthv font-size'>Chính quy</p>
+                                    <h3 className='details-khoa'>Khoa:</h3>
+                                    <p className='details-class-cthv font-size'>Kế toán - Kiểm toán</p>    
 
+                                </div>
+                                <div className='table-select-cthv'>
+                                    <h3 className='details-hvt'>MSSV:</h3>
+                                    <p className='details-class-cthv font-size'>171 002 1234</p>
+                                    <h3 className='details-cmnd'>Trạng thái:</h3>
+                                    <p className='details-class-cthv font-size'>Đang học</p>  
+                                    <h3 className='details-lhdt'>Chương trình đào tạo:</h3>
+                                    <p className='details-class-cthv font-size'>Đại trà</p>
+                                    <h3 className='details-khoa'>Ngành:</h3>
+                                    <p className='details-class-cthv font-size'>Kế toán</p>    
+
+                                </div>
+                                <div className='table-select-cthv'>
+                                    <h3 className='details-hvt'>MSSV:</h3>
+                                    <p className='details-class-cthv font-size'>171 002 1234</p>
+                                    <h3 className='details-cmnd'>Trạng thái:</h3>
+                                    <p className='details-class-cthv font-size'>Đang học</p>  
+                                    <h3 className='details-lhdt'>Chương trình đào tạo:</h3>
+                                    <p className='details-class-cthv font-size'>Đại trà</p>
+                                    <h3 className='details-khoa'>Ngành:</h3>
+                                    <p className='details-class-cthv font-size'>Kế toán</p>    
+
+                                </div>
+                                <div className='table-select-cthv'>
+                                    <h3 className='details-hvt'>MSSV:</h3>
+                                    <p className='details-class-cthv font-size'>171 002 1234</p>
+                                    <h3 className='details-cmnd'>Trạng thái:</h3>
+                                    <p className='details-class-cthv font-size'>Đang học</p>  
+                                    <h3 className='details-lhdt'>Chương trình đào tạo:</h3>
+                                    <p className='details-class-cthv font-size'>Đại trà</p>
+                                    <h3 className='details-khoa'>Ngành:</h3>
+                                    <p className='details-class-cthv font-size'>Kế toán</p>    
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/QLHP/Page/Pagecontent/CTHV.tsx
+++ src/components/QLHP/Page/Pagecontent/CTHV.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import CTHVien from '../CTHV/CTHVien'
+import CTHVTitle from '../Title/CTHVTitle'
 import User from '../User/User'
 
 export default function CTHV() {
   return (
     <div className="content">
     <User />
-    <CTHV />
+    <CTHVTitle />
     <CTHVien />
   </div>
   )
